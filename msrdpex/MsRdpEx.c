d346bc09d6ae31f21348b056b14b0b38be91c68a
Marc-André Moreau <marcandre.moreau@gmail.com>
2021-11-14 21:04:39 -0500

    add functor

--- msrdpex/MsRdpEx.c
+++ msrdpex/MsRdpEx.c
@@ -1,8 +1,4 @@
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdint.h>
-
 #include "MsRdpEx.h"
 
 HRESULT DllCanUnloadNow()
@@ -50,12 +46,63 @@ HRESULT DllLogoffClaimsToken(WCHAR* a1)
     return S_OK;
 }
 
+HRESULT DllCancelAuthentication()
+{
+    return S_OK;
+}
+
 HRESULT DllDeleteSavedCreds(WCHAR* a1, WCHAR* a2)
 {
     return S_OK;
 }
 
-HRESULT DllCancelAuthentication()
+// MsRdpEx_AxDll
+
+MsRdpEx_AxDll* MsRdpEx_AxDll_New(const char* filename)
 {
-    return S_OK;
+	HMODULE hModule;
+	MsRdpEx_AxDll* dll;
+
+	hModule = LoadLibraryA(filename);
+
+	if (!hModule)
+		return NULL;
+
+	dll = (MsRdpEx_AxDll*) malloc(sizeof(MsRdpEx_AxDll));
+
+	if (!dll)
+		return NULL;
+
+	ZeroMemory(dll, sizeof(MsRdpEx_AxDll));
+
+	dll->hModule = hModule;
+
+    dll->DllCanUnloadNow = (fnDllCanUnloadNow) GetProcAddress(hModule, "DllCanUnloadNow");
+    dll->DllGetClassObject = (fnDllGetClassObject) GetProcAddress(hModule, "DllGetClassObject");
+    dll->DllRegisterServer = (fnDllRegisterServer) GetProcAddress(hModule, "DllRegisterServer");
+    dll->DllUnregisterServer = (fnDllUnregisterServer) GetProcAddress(hModule, "DllUnregisterServer");
+    dll->DllGetTscCtlVer = (fnDllGetTscCtlVer) GetProcAddress(hModule, "DllGetTscCtlVer");
+    dll->DllSetAuthProperties = (fnDllSetAuthProperties) GetProcAddress(hModule, "DllSetAuthProperties");
+    dll->DllGetClaimsToken = (fnDllGetClaimsToken) GetProcAddress(hModule, "DllGetClaimsToken");
+    dll->DllSetClaimsToken = (fnDllSetClaimsToken) GetProcAddress(hModule, "DllSetClaimsToken");
+    dll->DllLogoffClaimsToken = (fnDllLogoffClaimsToken) GetProcAddress(hModule, "DllLogoffClaimsToken");
+    dll->DllCancelAuthentication = (fnDllCancelAuthentication) GetProcAddress(hModule, "DllCancelAuthentication");
+    dll->DllDeleteSavedCreds = (fnDllDeleteSavedCreds) GetProcAddress(hModule, "DllDeleteSavedCreds");
+
+	return dll;
+}
+
+void MsRdpEx_AxDll_Free(MsRdpEx_AxDll* dll)
+{
+    if (!dll)
+        return;
+    
+    if (dll->hModule) {
+        FreeLibrary(dll->hModule);
+        dll->hModule = NULL;
+    }
+
+    ZeroMemory(dll, sizeof(MsRdpEx_AxDll));
+    
+    free(dll);
 }
--- msrdpex/MsRdpEx.h
+++ msrdpex/MsRdpEx.h
@@ -1,15 +1,58 @@
 #ifndef MSRDPEX_H
 #define MSRDPEX_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
 #include <windows.h>
-
-
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
+typedef HRESULT (WINAPI * fnDllCanUnloadNow)(void);
 
+typedef HRESULT (WINAPI * fnDllGetClassObject)(REFCLSID rclsid, REFIID riid, LPVOID* ppv);
+
+typedef HRESULT (WINAPI * fnDllRegisterServer)(void);
+
+typedef HRESULT (WINAPI * fnDllUnregisterServer)(void);
+
+typedef uint64_t (WINAPI * fnDllGetTscCtlVer)(void);
+
+typedef HRESULT (WINAPI * fnDllSetAuthProperties)(uint64_t properties);
+
+typedef HRESULT (WINAPI * fnDllSetClaimsToken)(uint64_t a1, uint64_t a2, WCHAR* p3);
+
+typedef HRESULT (WINAPI * fnDllGetClaimsToken)(WCHAR* a1, WCHAR* a2, WCHAR* a3,
+    uint64_t a4, HWND a5, WCHAR** a6, WCHAR** a7, RECT* a8, WCHAR* a9);
+
+typedef HRESULT (WINAPI * fnDllLogoffClaimsToken)(WCHAR* a1);
+
+typedef HRESULT (WINAPI * fnDllCancelAuthentication)(void);
+
+typedef HRESULT (WINAPI * fnDllDeleteSavedCreds)(WCHAR* a1, WCHAR* a2);
+
+struct _MsRdpEx_AxDll
+{
+    HMODULE hModule;
+    fnDllCanUnloadNow DllCanUnloadNow;
+    fnDllGetClassObject DllGetClassObject;
+    fnDllRegisterServer DllRegisterServer;
+    fnDllUnregisterServer DllUnregisterServer;
+    fnDllGetTscCtlVer DllGetTscCtlVer;
+    fnDllSetAuthProperties DllSetAuthProperties;
+    fnDllGetClaimsToken DllGetClaimsToken;
+    fnDllSetClaimsToken DllSetClaimsToken;
+    fnDllLogoffClaimsToken DllLogoffClaimsToken;
+    fnDllCancelAuthentication DllCancelAuthentication;
+    fnDllDeleteSavedCreds DllDeleteSavedCreds;
+};
+typedef struct _MsRdpEx_AxDll MsRdpEx_AxDll;
+
+MsRdpEx_AxDll* MsRdpEx_AxDll_New(const char* filename);
+void MsRdpEx_AxDll_Free(MsRdpEx_AxDll* dll);
 
 #ifdef __cplusplus
 }
