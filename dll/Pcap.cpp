67cb8525e5d27332bac7935e4a039af1318a3a77
Marc-André Moreau <marcandre.moreau@gmail.com>
2022-02-02 15:31:51 -0500

    import pcap cooked capture utils

--- dll/Pcap.cpp
+++ dll/Pcap.cpp
@@ -1,4 +1,6 @@
 
+#include <MsRdpEx/Stream.h>
+
 #include <MsRdpEx/Pcap.h>
 
 #ifndef _WIN32
@@ -175,7 +177,7 @@ MsRdpEx_PcapFile* MsRdpEx_PcapFile_Open(const char* name, bool write)
         pcap->header.thiszone = 0;
         pcap->header.sigfigs = 0;
         pcap->header.snaplen = 0xFFFFFFFF;
-        pcap->header.network = 0;
+        pcap->header.network = 1; // ethernet
 
         if (!MsRdpEx_PcapFile_WriteHeader(pcap, &pcap->header))
             goto fail;
@@ -221,4 +223,237 @@ void MsRdpEx_PcapFile_Close(MsRdpEx_PcapFile* pcap)
 
     free(pcap->name);
     free(pcap);
+}
+
+static bool MsRdpEx_PcapFile_WriteEthernetHeader(MsRdpEx_PcapFile* pcap, PCAP_ETHERNET_HEADER* ethernet)
+{
+    MsRdpEx_Stream* s;
+    uint8_t buffer[14];
+    bool success = true;
+
+    if (!pcap || !pcap->fp || !ethernet)
+        return false;
+
+    s = MsRdpEx_Stream_New(buffer, 14, false);
+
+    if (!s)
+        return false;
+
+    MsRdpEx_StreamWrite(s, ethernet->Destination, 6);
+    MsRdpEx_StreamWrite(s, ethernet->Source, 6);
+    MsRdpEx_StreamWrite_UINT16_BE(s, ethernet->Type);
+
+    if (fwrite(buffer, 14, 1, pcap->fp) != 1)
+        success = false;
+    
+    MsRdpEx_Stream_Free(s);
+
+    return success;
+}
+
+static uint16_t IPv4Checksum(uint8_t* ipv4, int length)
+{
+    uint16_t tmp16;
+    uint32_t checksum = 0;
+
+    while (length > 1)
+    {
+        tmp16 = *((uint16_t*)ipv4);
+        checksum += tmp16;
+        length -= 2;
+        ipv4 += 2;
+    }
+
+    if (length > 0)
+        checksum += *ipv4;
+
+    while (checksum >> 16)
+        checksum = (checksum & 0xFFFF) + (checksum >> 16);
+
+    return (uint16_t)(~checksum);
+}
+
+static bool MsRdpEx_PcapFile_WriteIPv4Header(MsRdpEx_PcapFile* pcap, PCAP_IPV4_HEADER* ipv4)
+{
+    MsRdpEx_Stream* s;
+    uint8_t buffer[20];
+    int success = true;
+
+    if (!pcap || !pcap->fp || !ipv4)
+        return false;
+
+    s = MsRdpEx_Stream_New(buffer, 20, false);
+    
+    if (!s)
+        return false;
+
+    MsRdpEx_StreamWrite_UINT8(s, (ipv4->Version << 4) | ipv4->InternetHeaderLength);
+    MsRdpEx_StreamWrite_UINT8(s, ipv4->TypeOfService);
+    MsRdpEx_StreamWrite_UINT16_BE(s, ipv4->TotalLength);
+    MsRdpEx_StreamWrite_UINT16_BE(s, ipv4->Identification);
+    MsRdpEx_StreamWrite_UINT16_BE(s, (ipv4->InternetProtocolFlags << 13) | ipv4->FragmentOffset);
+    MsRdpEx_StreamWrite_UINT8(s, ipv4->TimeToLive);
+    MsRdpEx_StreamWrite_UINT8(s, ipv4->Protocol);
+    MsRdpEx_StreamWrite_UINT16(s, ipv4->HeaderChecksum);
+    MsRdpEx_StreamWrite_UINT32_BE(s, ipv4->SourceAddress);
+    MsRdpEx_StreamWrite_UINT32_BE(s, ipv4->DestinationAddress);
+    ipv4->HeaderChecksum = IPv4Checksum((uint8_t*)buffer, 20);
+    MsRdpEx_StreamRewind(s, 10);
+    MsRdpEx_StreamWrite_UINT16(s, ipv4->HeaderChecksum);
+    MsRdpEx_StreamSeek(s, 8);
+
+    if (fwrite(buffer, 20, 1, pcap->fp) != 1)
+        success = false;
+  
+    MsRdpEx_Stream_Free(s);
+    return success;
+}
+
+static bool MsRdpEx_PcapFile_WriteTcpHeader(MsRdpEx_PcapFile* pcap, PCAP_TCP_HEADER* tcp)
+{
+    MsRdpEx_Stream* s;
+    uint8_t buffer[20];
+    bool success = true;
+
+    if (!pcap || !pcap->fp || !tcp)
+        return false;
+
+    s = MsRdpEx_Stream_New(buffer, 20, false);
+    
+    if (!s)
+        return false;
+
+    MsRdpEx_StreamWrite_UINT16_BE(s, tcp->SourcePort);
+    MsRdpEx_StreamWrite_UINT16_BE(s, tcp->DestinationPort);
+    MsRdpEx_StreamWrite_UINT32_BE(s, tcp->SequenceNumber);
+    MsRdpEx_StreamWrite_UINT32_BE(s, tcp->AcknowledgementNumber);
+    MsRdpEx_StreamWrite_UINT8(s, (tcp->Offset << 4) | tcp->Reserved);
+    MsRdpEx_StreamWrite_UINT8(s, tcp->TcpFlags);
+    MsRdpEx_StreamWrite_UINT16_BE(s, tcp->Window);
+    MsRdpEx_StreamWrite_UINT16_BE(s, tcp->Checksum);
+    MsRdpEx_StreamWrite_UINT16_BE(s, tcp->UrgentPointer);
+
+    if (fwrite(buffer, 20, 1, pcap->fp) != 1)
+        success = false;
+
+    MsRdpEx_Stream_Free(s);
+    return success;
+}
+
+static UINT32 g_InboundSequenceNumber = 0;
+static UINT32 g_OutboundSequenceNumber = 0;
+
+bool MsRdpEx_PcapFile_PacketWrite(MsRdpEx_PcapFile* pcap, uint8_t* data, size_t length, uint32_t flags)
+{
+    PCAP_TCP_HEADER tcp;
+    PCAP_IPV4_HEADER ipv4;
+    struct timeval tp;
+    PCAP_RECORD record;
+    PCAP_ETHERNET_HEADER ethernet;
+
+    ethernet.Type = 0x0800;
+
+    if (!pcap || !pcap->fp)
+        return false;
+
+    if (flags & PCAP_PACKET_FLAG_OUTBOUND)
+    {
+        /* 00:15:5D:01:64:04 */
+        ethernet.Source[0] = 0x00;
+        ethernet.Source[1] = 0x15;
+        ethernet.Source[2] = 0x5D;
+        ethernet.Source[3] = 0x01;
+        ethernet.Source[4] = 0x64;
+        ethernet.Source[5] = 0x04;
+        /* 00:15:5D:01:64:01 */
+        ethernet.Destination[0] = 0x00;
+        ethernet.Destination[1] = 0x15;
+        ethernet.Destination[2] = 0x5D;
+        ethernet.Destination[3] = 0x01;
+        ethernet.Destination[4] = 0x64;
+        ethernet.Destination[5] = 0x01;
+    }
+    else
+    {
+        /* 00:15:5D:01:64:01 */
+        ethernet.Source[0] = 0x00;
+        ethernet.Source[1] = 0x15;
+        ethernet.Source[2] = 0x5D;
+        ethernet.Source[3] = 0x01;
+        ethernet.Source[4] = 0x64;
+        ethernet.Source[5] = 0x01;
+        /* 00:15:5D:01:64:04 */
+        ethernet.Destination[0] = 0x00;
+        ethernet.Destination[1] = 0x15;
+        ethernet.Destination[2] = 0x5D;
+        ethernet.Destination[3] = 0x01;
+        ethernet.Destination[4] = 0x64;
+        ethernet.Destination[5] = 0x04;
+    }
+
+    ipv4.Version = 4;
+    ipv4.InternetHeaderLength = 5;
+    ipv4.TypeOfService = 0;
+    ipv4.TotalLength = (uint16_t)(length + 20 + 20);
+    ipv4.Identification = 0;
+    ipv4.InternetProtocolFlags = 0x02;
+    ipv4.FragmentOffset = 0;
+    ipv4.TimeToLive = 128;
+    ipv4.Protocol = 6; /* TCP */
+    ipv4.HeaderChecksum = 0;
+
+    if (flags & PCAP_PACKET_FLAG_OUTBOUND)
+    {
+        ipv4.SourceAddress = 0xC0A80196;      /* 192.168.1.150 */
+        ipv4.DestinationAddress = 0x4A7D64C8; /* 74.125.100.200 */
+    }
+    else
+    {
+        ipv4.SourceAddress = 0x4A7D64C8;      /* 74.125.100.200 */
+        ipv4.DestinationAddress = 0xC0A80196; /* 192.168.1.150 */
+    }
+
+    tcp.SourcePort = 3389;
+    tcp.DestinationPort = 3389;
+
+    if (flags & PCAP_PACKET_FLAG_OUTBOUND)
+    {
+        tcp.SequenceNumber = g_OutboundSequenceNumber;
+        tcp.AcknowledgementNumber = g_InboundSequenceNumber;
+        g_OutboundSequenceNumber += length;
+    }
+    else
+    {
+        tcp.SequenceNumber = g_InboundSequenceNumber;
+        tcp.AcknowledgementNumber = g_OutboundSequenceNumber;
+        g_InboundSequenceNumber += length;
+    }
+
+    tcp.Offset = 5;
+    tcp.Reserved = 0;
+    tcp.TcpFlags = 0x0018;
+    tcp.Window = 0x7FFF;
+    tcp.Checksum = 0;
+    tcp.UrgentPointer = 0;
+    record.data = data;
+    record.length = length;
+    record.header.incl_len = record.length + 14 + 20 + 20;
+    record.header.orig_len = record.length + 14 + 20 + 20;
+    record.next = NULL;
+
+    gettimeofday(&tp, 0);
+    record.header.ts_sec = tp.tv_sec;
+    record.header.ts_usec = tp.tv_usec;
+
+    if (!MsRdpEx_PcapFile_WriteRecordHeader(pcap, &record.header) ||
+        !MsRdpEx_PcapFile_WriteEthernetHeader(pcap, &ethernet) ||
+        !MsRdpEx_PcapFile_WriteIPv4Header(pcap, &ipv4) ||
+        !MsRdpEx_PcapFile_WriteTcpHeader(pcap, &tcp) ||
+        !MsRdpEx_PcapFile_WriteRecord(pcap, &record)) {
+        return false;
+    }
+
+    MsRdpEx_PcapFile_Flush(pcap);
+
+    return true;
 }
--- include/MsRdpEx/Pcap.h
+++ include/MsRdpEx/Pcap.h
@@ -40,6 +40,53 @@ struct _pcap_record
 
 typedef struct _MsRdpEx_Pcap MsRdpEx_PcapFile;
 
+#pragma pack(push, 1)
+
+struct _PCAP_ETHERNET_HEADER
+{
+    uint8_t Destination[6];
+    uint8_t Source[6];
+    uint16_t Type;
+};
+typedef struct _PCAP_ETHERNET_HEADER PCAP_ETHERNET_HEADER;
+
+struct _PCAP_IPV4_HEADER
+{
+    uint8_t Version;
+    uint8_t InternetHeaderLength;
+    uint8_t TypeOfService;
+    uint16_t TotalLength;
+    uint16_t Identification;
+    uint8_t InternetProtocolFlags;
+    uint16_t FragmentOffset;
+    uint8_t TimeToLive;
+    uint8_t Protocol;
+    uint16_t HeaderChecksum;
+    uint32_t SourceAddress;
+    uint32_t DestinationAddress;
+};
+typedef struct _PCAP_IPV4_HEADER PCAP_IPV4_HEADER;
+
+struct _PCAP_TCP_HEADER
+{
+    uint16_t SourcePort;
+    uint16_t DestinationPort;
+    uint32_t SequenceNumber;
+    uint32_t AcknowledgementNumber;
+    uint8_t Offset;
+    uint8_t Reserved;
+    uint8_t TcpFlags;
+    uint16_t Window;
+    uint16_t Checksum;
+    uint16_t UrgentPointer;
+};
+typedef struct _PCAP_TCP_HEADER PCAP_TCP_HEADER;
+
+#pragma pack(pop)
+
+#define PCAP_PACKET_FLAG_INBOUND        0x00000001
+#define PCAP_PACKET_FLAG_OUTBOUND       0x00000002
+
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -53,6 +100,8 @@ bool MsRdpEx_PcapFile_GetNextRecord(MsRdpEx_PcapFile* pcap, PCAP_RECORD* record)
 bool MsRdpEx_PcapFile_GetNextRecordHeader(MsRdpEx_PcapFile* pcap, PCAP_RECORD* record);
 bool MsRdpEx_PcapFile_GetNextRecordContent(MsRdpEx_PcapFile* pcap, PCAP_RECORD* record);
 void MsRdpEx_PcapFile_Flush(MsRdpEx_PcapFile* pcap);
+
+bool MsRdpEx_PcapFile_PacketWrite(MsRdpEx_PcapFile* pcap, uint8_t* data, size_t length, uint32_t flags);
 
 #ifdef __cplusplus
 }
