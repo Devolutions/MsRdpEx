da320fe4f34ea763be9e4df47c7790e1a498e399
Marc-André Moreau <marcandre.moreau@gmail.com>
2022-03-25 15:21:08 -0400

    use singleton session manager

---
+++ dotnet/MsRdpEx_App/RdpManager.cs
@@ -0,0 +1,64 @@
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+using MsRdpEx;
+
+namespace MsRdpEx_App
+{
+    public class MsRdpExManager
+    {
+        private static readonly RdpCoreApi coreApi;
+
+        private static bool axHookEnabled = false;
+
+        public RdpCoreApi CoreApi { get => coreApi; }
+
+        public bool AxHookEnabled { get => axHookEnabled; }
+
+        private static RdpCoreApi LoadCoreApi()
+        {
+            bool axHookEnabled = true;
+
+            RdpCoreApi coreApi = new RdpCoreApi();
+
+            string logFilePath = Environment.ExpandEnvironmentVariables("%LocalAppData%\\MsRdpEx\\HostApp.log");
+            string pcapFilePath = Environment.ExpandEnvironmentVariables("%LocalAppData%\\MsRdpEx\\capture.pcap");
+
+            coreApi.LogEnabled = true;
+            coreApi.LogLevel = MsRdpEx_LogLevel.Debug;
+            coreApi.LogFilePath = logFilePath;
+            coreApi.PcapEnabled = false;
+            coreApi.PcapFilePath = pcapFilePath;
+            coreApi.AxHookEnabled = axHookEnabled;
+            coreApi.Load();
+
+            return coreApi;
+        }
+
+        private static MsRdpExManager instance = null;
+        private static readonly object padlock = new object();
+
+        public static MsRdpExManager Instance
+        {
+            get
+            {
+                lock (padlock)
+                {
+                    if (instance == null)
+                    {
+                        instance = new MsRdpExManager();
+                    }
+
+                    return instance;
+                }
+            }
+        }
+
+        static MsRdpExManager()
+        {
+            coreApi = LoadCoreApi();
+        }
+    }
+}
