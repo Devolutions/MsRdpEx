bbab376f130289617352a56d48d1d57e15887706
Marc-André Moreau <marcandre.moreau@gmail.com>
2023-11-22 15:26:45 -0500

    use mstsc.exe, msrdc.exe executable wrappers

--- exe/MsRdpEx.c
+++ exe/MsRdpEx.c
@@ -1,11 +1,29 @@
 
 #include <MsRdpEx/RdpProcess.h>
 
-int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, int nShowCmd)
+int WINAPI wWinMain(
+    _In_ HINSTANCE hInstance,
+    _In_opt_ HINSTANCE hPrevInstance,
+    _In_ LPWSTR lpCmdLine,
+    _In_ int nShowCmd)
 {
     HRESULT hr;
+    const char* axName = NULL;
+    char moduleFilePath[MSRDPEX_MAX_PATH];
 
-    hr = MsRdpEx_LaunchProcess(-1, NULL, NULL, NULL);
+    if (!GetEnvironmentVariableA("MSRDPEX_AXNAME", NULL, 0))
+    {
+        axName = "mstsc";
+
+        if (GetModuleFileNameA(NULL, moduleFilePath, MSRDPEX_MAX_PATH))
+        {
+            if (strstr(moduleFilePath, "msrdc.exe")) {
+                axName = "msrdc";
+            }
+        }
+    }
+
+    hr = MsRdpEx_LaunchProcess(-1, NULL, NULL, axName);
 
     return 0;
 }
