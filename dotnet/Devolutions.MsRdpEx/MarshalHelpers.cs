9cddc6615e03519439d8930df571d2e5bac75164
Marc-André Moreau <marcandre.moreau@gmail.com>
2024-01-08 10:56:52 -0500

    fix UTF-8 string marshalling in C# bindings

--- dotnet/Devolutions.MsRdpEx/MarshalHelpers.cs
+++ dotnet/Devolutions.MsRdpEx/MarshalHelpers.cs
@@ -1,24 +1,35 @@
 using System;
+using System.IO;
 using System.Net.NetworkInformation;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using System.Text;
 
 namespace MsRdpEx
 {
     public class MarshalHelpers
     {
-        // Replacement for Marshal.PtrToStringUTF8
-        public static string PtrToStringUTF8(IntPtr ptr)
+        // Replacement for native strlen() function
+        public static unsafe int PtrStringLength(IntPtr ptr)
         {
+            byte* p = (byte*)ptr;
             int length = 0;
-            while (Marshal.ReadByte(ptr, length) != 0)
+
+            while (*p != 0)
             {
+                p++;
                 length++;
             }
 
+            return length;
+        }
+
+        // Replacement for Marshal.PtrToStringUTF8
+        public static string PtrToStringUTF8(IntPtr ptr)
+        {
+            int length = PtrStringLength(ptr);
             byte[] buffer = new byte[length];
             Marshal.Copy(ptr, buffer, 0, length);
-
             return System.Text.Encoding.UTF8.GetString(buffer);
         }
 
@@ -36,6 +47,39 @@ namespace MsRdpEx
             Marshal.WriteByte(unmanagedMemory, utf8Bytes.Length, 0);
 
             return unmanagedMemory;
+        }
+
+        // Replacement for [MarshalAs(UnmanagedType.LPStr)]
+        // [MarshalAs(UnmanagedType.CustomMarshaler, MarshalTypeRef = typeof(MarshalHelpers.LPUTF8Str))]
+
+        public class LPUTF8Str : ICustomMarshaler
+        {
+            public void CleanUpManagedData(object ManagedObj)
+            {
+
+            }
+
+            public void CleanUpNativeData(IntPtr pNativeData)
+            {
+                Marshal.FreeCoTaskMem(pNativeData);
+            }
+
+            public int GetNativeDataSize()
+            {
+                throw new NotImplementedException();
+            }
+
+            public IntPtr MarshalManagedToNative(object ManagedObj)
+            {
+                return MarshalHelpers.StringToCoTaskMemUTF8((string)ManagedObj);
+            }
+
+            public object MarshalNativeToManaged(IntPtr pNativeData)
+            {
+                return MarshalHelpers.PtrToStringUTF8(pNativeData);
+            }
+
+            public static ICustomMarshaler GetInstance(string cookie) => new LPUTF8Str();
         }
     }
 }
