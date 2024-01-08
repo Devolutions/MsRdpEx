f9a037091c4d1aec2700c5e5a82b6e6178c4d2d3
Marc-André Moreau <marcandre.moreau@gmail.com>
2024-01-08 15:22:14 -0500

    start multi-targeting net48 and net8.0-windows

--- dotnet/AxInterop.MSTSCLib/RdpAxHost.cs
+++ dotnet/AxInterop.MSTSCLib/RdpAxHost.cs
@@ -113,7 +113,7 @@ namespace AxMSTSCLib {
     public class AxHostEx : System.Windows.Forms.AxHost
     {
         public string axName = "mstsc";
-        public string rdpExDll = null;
+        public string rdpExDll = string.Empty;
         private static object loadLock = new object();
 
         public static string RdpGetAxDllPath(string axName)
@@ -147,7 +147,7 @@ namespace AxMSTSCLib {
 
         public static object RdpGetClassObject(Guid clsid, string axName, string rdpExDll)
         {
-            object obj = null;
+            object? obj = null;
 
             lock (loadLock)
             {
