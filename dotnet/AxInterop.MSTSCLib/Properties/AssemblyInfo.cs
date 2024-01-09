9be58aa4a91a49191c7a53c8344f7c588199bc8b
Marc-André Moreau <marcandre.moreau@gmail.com>
2024-01-09 10:02:55 -0500

    set net48 as target framework Interop.MSTSCLib.dll

--- dotnet/AxInterop.MSTSCLib/Properties/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-using System.Diagnostics;
-using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Windows.Forms;
-
-[assembly: AxHost.TypeLibraryTimeStamp("11/09/2021 16:17:20")]
-[assembly: AssemblyVersion("1.0.0.0")]
